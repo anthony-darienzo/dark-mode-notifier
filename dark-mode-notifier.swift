8ccb8f60f6dd3c5c909c1854e70e2040d92fce6f
Anthony D'Arienzo <anthony384@outlook.com>
2022-07-23 19:23:23 -0500

    Modified source to omit wezterm and log more.

--- dark-mode-notifier.swift
+++ dark-mode-notifier.swift
@@ -11,18 +11,12 @@ import os.log
 /* Takes three arguments:
  * [0]: path to executable (as usual)
  * [1]: target config file
- * [2]: path to light colorscheme
- * [3]: path to dark colorscheme
- * [4]: path to wezterm config file
- * [5]: command to tell neovim to update colors
+ * [2]: command to tell neovim to update colors
  */
 
 let ARGUMENTS = CommandLine.arguments
 let CONFIG_PATH = ARGUMENTS[1]
-let LIGHT_SOURCE = ARGUMENTS[2]
-let DARK_SOURCE = ARGUMENTS[3]
-let WEZTERM_PATH = ARGUMENTS[4]
-let NEOVIM_NOTIFY = ARGUMENTS[5]
+let NEOVIM_NOTIFY = ARGUMENTS[2]
 
 let mainLog = Logger(
     subsystem:"local.dark-mode-notifier",
@@ -31,35 +25,20 @@ let mainLog = Logger(
 
 func writeConfigFile(isDark: Bool) throws {
     let path = URL(fileURLWithPath: CONFIG_PATH)
-    let wez_path = URL(fileURLWithPath: WEZTERM_PATH)
     
-    let target_colors: String
-    if isDark {
-        target_colors = DARK_SOURCE
-    } else {
-        target_colors = LIGHT_SOURCE
-    }
-
-    let configString = """
-    import:
-        - \(target_colors)
+    let configString = 
     """
-    
-    let weztermString = """
-        -- return true if dark mode
-        return \( String(isDark) ) 
+    -- return true if dark mode
+    return \(String(isDark)) 
     """
     
     if let configStringData = configString.data(using: .utf8) {
         try? configStringData.write(to: path)
     }
 
-    if let weztermStringData = weztermString.data(using: .utf8) {
-        try? weztermStringData.write(to: wez_path)
-    }
 }
 
-func updateAlacrittyTheme() {
+func updateConfigPath() {
     let isDark = 
         UserDefaults.standard.string(forKey: "AppleInterfaceStyle") == "Dark"
     try? writeConfigFile(isDark: isDark) 
@@ -84,7 +63,7 @@ func notifyNeovim() {
     mainLog.info("Notified neovim, received msg: \(output, privacy: .public)")
 }
 
-updateAlacrittyTheme() // Call once so that theme updates when system logs in.
+updateConfigPath() // Call once so that theme updates when system logs in.
 notifyNeovim()
 
 // Listen for when the colorscheme changes
@@ -94,7 +73,17 @@ let themeObserver = DistributedNotificationCenter.default.addObserver(
     queue: nil, 
     using: { 
         (Notification) -> Void in
-            updateAlacrittyTheme()
+            mainLog.info("local.dark-mode-notifier detected theme change!")
+            updateConfigPath()
+            notifyNeovim()
+    }
+)
+let wakeObserver = DistributedNotificationCenter.default.addObserver(
+    forName: NSWorkspace.didWakeNotification, 
+    object: nil,
+    queue: nil, 
+    using: { 
+        (Notification) -> Void in
             notifyNeovim()
     }
 )
